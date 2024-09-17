40cdd272c33687bf01cd9005b3ef77117b9d66ff
Pasha <yashchenkops@gmail.com>
2024-09-17 16:43:52 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,14 @@
 const initIPLocation = () => {
   if (window.location.hostname !== 'localhost') {
     fetch('https://ipapi.co/json/')
-      .then(response => response.json())
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
       .then(data => {
         const countryCode = data.country_code;
-
         const currentPath = window.location.pathname;
 
         if (countryCode === 'FR' && currentPath !== '/fr.html') {
@@ -14,7 +18,7 @@ const initIPLocation = () => {
         }
       })
       .catch(error => {
-        console.error('Error getting an IP address:', error);
+        console.error('Error getting IP location:', error);
         if (window.location.pathname !== '/index.html') {
           window.location.href = '/index.html';
         }
